bfd3bd5a720cc267ac6081b027cc3f86dc87aa06
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-10 17:39:39 -0500

    Add loader

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,15 +1,23 @@
 import React, { useState, useEffect } from "react";
 import Category from "../Category";
 
-export default function ListOfCategories() {
+function useCategoriesData() {
   const [categories, setCategories] = useState([]);
-  const [showFixed, setShowFixed] = useState(false);
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     fetch("https://petgram-api-grajalesu.vercel.app/categories")
       .then((res) => res.json())
-      .then((res) => setCategories(res));
+      .then((res) => setCategories(res))
+      .finally(() => setLoading(false));
   }, []);
+
+  return { categories, loading };
+}
+
+export default function ListOfCategories() {
+  const [showFixed, setShowFixed] = useState(false);
+  const { categories, loading } = useCategoriesData();
 
   useEffect(() => {
     const onScroll = (e) => {
@@ -22,8 +30,20 @@ export default function ListOfCategories() {
     return () => document.removeEventListener("scroll", onScroll);
   }, [showFixed]);
 
+  if (loading) {
+    return (
+      <ul className="flex overflow-scroll overflow-y-hidden w-full mb-3">
+        {[1, 2, 3, 4, 5, 6].map((loadElement) => (
+          <li className="px-2" key={loadElement}>
+            <Category />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
   const fixedCategory = () => (
-    <ul className="flex fixed overflow-scroll overflow-y-hidden w-full bg-white rounded-[60px] shadow-md shadow-gray-100 left-0 right-0 mx-auto max-w-[400px] p-2 top-0 scale-[0.7] z-10">
+    <ul className="flex fixed overflow-scroll overflow-y-hidden w-full bg-white rounded-[60px] shadow-md shadow-gray-100 left-0 right-0 mx-auto max-w-[450px] p-2 top-0 scale-[0.7] z-10">
       {categories.map((category) => (
         <li className="px-2" key={category.id}>
           <Category {...category} />
