e4cfcb3672aadac730eb36a8f6571c6cea72e549
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-10 14:15:59 -0500

    Add Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ListOfCategories from "./components/ListOfCategories";
-
+import ListOfPhotoCards from "./components/ListOfPhotoCards";
+import Logo from "./components/Logo";
 function App() {
   return (
     <div>
+      <Logo />
       <ListOfCategories />
+      <ListOfPhotoCards />
     </div>
   );
 }
