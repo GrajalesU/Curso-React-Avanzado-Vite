b88cf7308504076cd81d692fd8b63952215b7cb1
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-10 14:16:49 -0500

    Add categories from DB

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Category from "../Category";
+import { categories } from "./db.json";
 export default function ListOfCategories() {
   return (
     <ul className="flex overflow-scroll w-full">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((category) => (
-        <li className="px-2" key={category}>
-          <Category />
+      {categories.map((category) => (
+        <li className="px-2" key={category.id}>
+          <Category {...category} />
         </li>
       ))}
     </ul>
