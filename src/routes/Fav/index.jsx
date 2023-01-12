136c82e93dd48a29b144d68bf71e29932c518993
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-12 16:49:38 -0500

    Add title style

--- src/routes/Fav/index.jsx
+++ src/routes/Fav/index.jsx
@@ -3,7 +3,7 @@ import Favorites from "../../components/Favorites";
 export default function Fav() {
   return (
     <div>
-      <h1>Favorites</h1>
+      <h1 className="text-lg ml-4 font-semibold">Favorites</h1>
       <Favorites />
     </div>
   );
